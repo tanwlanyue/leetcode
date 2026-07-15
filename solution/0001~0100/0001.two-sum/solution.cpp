fa608bc0b43f91205720b8d6c056b123521bba67
tanwlanyue <tanwlanyue@gmail.com>
2026-07-15 11:01:14 +0800

    0001.two-sum

---
+++ solution/0001~0100/0001.two-sum/solution.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        map.reserve(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            auto it = map.find(target - num);
+            if (it != map.end()) {
+                return {it->second, i};
+            }  
+            map[num] = i;
+        }
+        return {}; 
+    } 
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums{2,7,11,15};
+    vector<int> res = solution.twoSum(nums,9);
+    if (res.size() == 2) {
+        cout << res[0] << "," << res[1] << endl;
+    }
+}
