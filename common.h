fbbad901d121e2b4afe31c0da50e1a85a2ee8b25
tanwlanyue <tanwlanyue@gmail.com>
2026-07-16 17:01:21 +0800

    0002.add-two-numbers

--- common.h
+++ common.h
@@ -12,4 +12,25 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+ListNode* CreateListNodeByVector(const vector<int>& nums)
+{
+    ListNode* head = new ListNode(); 
+    ListNode* dummy = head;
+    for (const auto& num : nums) {
+        dummy->next = new ListNode(num);
+        dummy = dummy->next;
+    }
+    return head->next;
+}
+
+void PrintListNode(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        head = head->next;
+        if (head) cout << ",";
+    }
+    cout << endl;
+}
+
 #endif
--- solution/0001~0100/0002.add-two-numbers/solution.cpp
+++ solution/0001~0100/0002.add-two-numbers/solution.cpp
@@ -3,10 +3,25 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return nullptr;
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        return dummy.next;
     }
 };
 
 int main() {
-
+    ListNode* l1 = CreateListNodeByVector(vector{9,9,9,9,9,9,9});
+    ListNode* l2 = CreateListNodeByVector(vector{9,9,9,9});
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+    PrintListNode(result);
 }
