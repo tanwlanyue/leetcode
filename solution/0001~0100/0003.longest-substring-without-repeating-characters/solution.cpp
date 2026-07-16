83e2d36b669efbd0e202ee2a18269099590ba372
tanwlanyue <tanwlanyue@gmail.com>
2026-07-16 17:23:52 +0800

    0003.longest-substring-without-repeating-characters

--- solution/0001~0100/0003.longest-substring-without-repeating-characters/solution.cpp
+++ solution/0001~0100/0003.longest-substring-without-repeating-characters/solution.cpp
@@ -3,10 +3,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return 0;
+        vector<int> last(128, -1);
+        int len = s.length();
+        int j = 0;
+        int i = -1;
+        int maxLen = 0;
+        while (j < len) {
+            char c = s.at(j);
+            if (last[c] != -1) {
+                i = max(i, last[c]);
+            }
+            maxLen = max(maxLen, j - i);
+            last[c] = j;
+            j++;
+        }
+
+        return maxLen;
     }
 };
 
 int main() {
-
+    Solution solution;
+    string s = "abcabcbb";
+    cout << solution.lengthOfLongestSubstring(s) << endl;
+    s = "bbbbb";
+    cout << solution.lengthOfLongestSubstring(s) << endl;
+    s = "pwwkew";
+    cout << solution.lengthOfLongestSubstring(s) << endl;
 }
